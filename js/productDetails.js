95d1e1ffc0b168187e384cb326a03c14789a2e1d
Laimonas Dirgėlas <102238163+DeAngelo777@users.noreply.github.com>
2024-08-14 17:50:12 +0300

    up

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,17 +1,3 @@
-
-export function productDetails(list, id) {
-
-
-if ((Object.keys(list).length) === 0) {
-return `Prekė, su ID: ${id} neegzistuoja. Krepšelis yra tuščias`;
-} else if (!Number.isInteger(id)) {
-    return  'Erorr: Įveskite sveikajį skaičiu';
-
-} else if ((list[0].id) === id){
-    return('------------------------------\nPrekės informacija\n------------------------------\nID            | ' + list[0].id + '\nPavadinimas   | ' + list[0].name + '\nKiekis        | '
-     + list[0].amount + ' vnt\nVieneto kaina | ' +  list[0].unitPrice/100 + ' Eur\nViso mokėti   | ' + (list[0].amount * list[0].unitPrice/100).toFixed(2) + ' Eur\n------------------------------'); 
-    } else { 
-     
-    return (`Prekė, su ID: ${id} neegzistuoja.`);
-}
-}
+module.exports = function(details, id) {
+    return "nieko.";
+};
--- js/shoppingList.js
+++ js/shoppingList.js
@@ -1,9 +1,3 @@
-export function shoppingList(list, id) {
-    if (Object.keys(list).length === 0 ) {
-        console.log('Šiuo metu, jūsų prekių krepšelis yra tuščias.')
-    } else {
-
-      
-return ('Jūsų prekių krepšelyje yra ' + (Object.keys(list).length) + ' prekes\n-----------------------------------------------------------\nPavadinimas | Kiekis  | Vieneto kaina | Viso mokėti\n-----------------------------------------------------------\n1 ' + (list[0].name) + ' | ' + (list[0].amount) + ' | ' + (list[0].unitPrice/100) + ' Eur' + '      | ' +  ((list[0].amount) * ((list[0].unitPrice/100).toFixed(2))) + ' Eur\n2 ' + (list[0].name));
-    }
-}
+module.exports = function(list) {
+    return "Duomenų nėra.";
+};
