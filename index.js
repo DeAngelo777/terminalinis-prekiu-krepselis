c2d8897b9f176fcc316ab71affcadb084a1ef7d3
Laimonas Dirgėlas <102238163+DeAngelo777@users.noreply.github.com>
2024-08-14 17:54:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Vieta reikiamų failų importavimui... jei reikia tokių 👀
 
 /*
 --------------------------------------------------------
@@ -13,22 +14,12 @@ Vienintelė išimtis, kada gali nesutapti spausdinamas rezultatas, jei kurdamas 
 
 const emptyList = [];
 
-// console.log(shoppingList(emptyList));
+console.log(shoppingList(emptyList));
 /*
 Šiuo metu, jūsų prekių krepšelis yra tuščias.
 */
 
-const singleProductShoppingList2 = [
-    {
-        id: 2,
-        name: 'Kivi',
-        amount: 23,
-        unitPrice: 14,
-    },
-
-];
-
-// console.log(productDetails(emptyList, 42069));
+console.log(productDetails(emptyList, 42069));
 /*
 Prekė, su ID: 42069 neegzistuoja.
 */
@@ -54,58 +45,7 @@ const firstShoppingList = [
     },
 ];
 
-
-
-const ids = []
-singleProductShoppingList2.forEach(({id}) => ids.push(id) )
-
-console.log(ids);
-
-
-// console.log(firstShoppingList[0].id);
-
-
-// for (let i = 0; i < firstShoppingList.length; i++){
-
-
-//     if (firstShoppingList[1].id === i)
-        
-//     console.log(i);
-// }
-
-
-
-
-
-
-
-
-
-
-
-/*
-Prekė, su ID: 42069 neegzistuoja.
-*/
-
-// Padaryta
-// console.log(firstShoppingList.length);
-// console.log(firstShoppingList[0].name);
-// console.log();
-// function shoppingList(list,id){
-
-//     console.log('Jūsų prekių krepšelyje yra ' + list.length + ' prekes');
-//     console.log(`-----------------------------------------------------------`);
-// console.log('Pavadinimas | Kiekis  | Vieneto kaina | Viso mokėti');
-//     console.log(`-----------------------------------------------------------`);
-//     console.log('1 ' + list[0].name + ' | ' + list[0].amount + ' | ' + list[0].unitPrice/100 + ' Eur' + '      | ' +  (list[0].amount * list[0].unitPrice/100).toFixed(2) + ' Eur');
-//     console.log('2 ' + list[1].name + '   | ' + list[1].amount + ' vnt   | ' + list[1].unitPrice/100 + ' Eur' + '       | ' +  (list[1].amount * list[1].unitPrice/100).toFixed(2) + ' Eur');
-//     console.log('3 ' + list[2].name + '  | ' + list[2].amount + ' vnt   | ' + list[2].unitPrice/100 + ' Eur' + '      | ' +  (list[2].amount * list[2].unitPrice/100).toFixed(2) + ' Eur');
-//     console.log(`-----------------------------------------------------------`);
-// console.log();
-// }
-
-
-// console.log(shoppingList(firstShoppingList));
+console.log(shoppingList(firstShoppingList));
 /*
 Jūsų prekių krepšelyje yra 3 prekės:
 -----------------------------------------------------------
@@ -117,52 +57,12 @@ Pavadinimas  | Kiekis      | Vieneto kaina | Viso mokėti
 -----------------------------------------------------------
 */
 
+console.log(productDetails(firstShoppingList, 42069));
+/*
+Prekė, su ID: 42069 neegzistuoja.
+*/
 
-// Prekės informacija
-// ------------------------------
-// ID            | 1
-// Pavadinimas   | Pomidoras
-// Kiekis        | 1000000 vnt
-// Vieneto kaina | 1.99 Eur
-// Viso mokėti   | 1990000.00 Eur
-// ------------------------------
-// */
-
-
-// function productDetails(list, id) {
-// if ((list[0].id) === id) {
-// return ('------------------------------\nPrekės informacija\n------------------------------\nID            | ' + firstShoppingList[0].id + '\nPavadinimas   | ' + firstShoppingList[0].name + '\nKiekis        | '
-//      + firstShoppingList[0].amount + ' vnt\nVieneto kaina | ' +  firstShoppingList[0].unitPrice/100 + ' Eur\nViso mokėti   | ' + (list[0].amount * list[0].unitPrice/100).toFixed(2) + ' Eur\n------------------------------') 
-
-
-    
-//       } else if ((list[1].id) === id) {
-//         return ('------------------------------\nPrekės informacija\n------------------------------\nID            | ' + firstShoppingList[1].id + '\nPavadinimas   | ' + firstShoppingList[1].name + '\nKiekis        | '
-//             + firstShoppingList[1].amount + ' vnt\nVieneto kaina | ' +  firstShoppingList[1].unitPrice/100 + ' Eur\nViso mokėti   | ' + (list[1].amount * list[1].unitPrice/100).toFixed(2) + ' Eur\n------------------------------') 
-            
-
-
-
-// } 
-
-
-
-
-// else if ((list[2].id) === id) {
-//     return ('------------------------------\nPrekės informacija\n------------------------------\nID            | ' + firstShoppingList[2].id + '\nPavadinimas   | ' + firstShoppingList[2].name + '\nKiekis        | '
-//         + firstShoppingList[2].amount + ' vnt\nVieneto kaina | ' +  firstShoppingList[2].unitPrice/100 + ' Eur\nViso mokėti   | ' + (list[2].amount * list[2].unitPrice/100).toFixed(2) + ' Eur\n------------------------------') 
-        
-
-
-
-// } 
-    
-    
-//     else {
-//         console.log(`Prekė, su ID: ${id} neegzistuoja.`);
-//       }
-//     }
-// console.log(productDetails(firstShoppingList, 7));
+console.log(productDetails(firstShoppingList, 1));
 /*
 ------------------------------
 Prekės informacija
@@ -175,7 +75,7 @@ Viso mokėti   | 1990000.00 Eur
 ------------------------------
 */
 
-// console.log(productDetails(firstShoppingList, 3));
+console.log(productDetails(firstShoppingList, 3));
 /*
 ------------------------
 Prekės informacija
@@ -188,7 +88,7 @@ Viso mokėti   | 1.00 Eur
 ------------------------
 */
 
-// console.log(productDetails(firstShoppingList, 7));
+console.log(productDetails(firstShoppingList, 7));
 /*
 ------------------------
 Prekės informacija
@@ -201,19 +101,6 @@ Viso mokėti   | 0.45 Eur
 ------------------------
 */
 
-
-// console.log(firstShoppingList[0].name);
-// function shoppingList(list,id){
-
-//     console.log('Jūsų prekių krepšelyje yra ' + list.length + ' prekes');
-//     console.log(`-----------------------------------------------------------`);
-// console.log('Pavadinimas | Kiekis  | Vieneto kaina | Viso mokėti');
-//     console.log(`-----------------------------------------------------------`);
-//     console.log('1 ' + list[0].name + ' | ' + list[0].amount + ' | ' + list[0].unitPrice/100 + ' Eur' + '      | ' +  (list[0].amount * list[0].unitPrice/100).toFixed(2) + ' Eur');
-//     console.log(`-----------------------------------------------------------`);
-// }
-
-
 const singleProductShoppingList = [
     {
         id: 2,
@@ -221,27 +108,9 @@ const singleProductShoppingList = [
         amount: 23,
         unitPrice: 14,
     },
-
 ];
 
-
-// console.log(shoppingList(singleProductShoppingList));
-
-
-
-
-        // } if (i == id) {
-        //    return console.log('tinka');
-        
-        // } else 
-        
-        // console.log('netinka');
-
-
-
-
-
-
+console.log(shoppingList(singleProductShoppingList));
 /*
 Jūsų prekių krepšelyje yra 1 prekė:
 --------------------------------------------------
@@ -251,12 +120,12 @@ Pavadinimas | Kiekis | Vieneto kaina | Viso mokėti
 --------------------------------------------------
 */
 
-// console.log(productDetails(singleProductShoppingList, 42069));
+console.log(productDetails(singleProductShoppingList, 42069));
 /*
 Prekė, su ID: 42069 neegzistuoja.
 */
 
-// console.log(productDetails(singleProductShoppingList, 2));
+console.log(productDetails(singleProductShoppingList, 2));
 /*
 ------------------------
 Prekės informacija
