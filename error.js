046c8da5f21622f4a295d449b116e14330f1673b
Laimonas Dirgėlas <102238163+DeAngelo777@users.noreply.github.com>
2024-08-14 17:51:49 +0300

    Delete error.js

--- error.js
+++
@@ -1,8 +0,0 @@
-// function productDetails(list, id) {
-
-//     for (let i = 0; i < list.length; i++){
-        
-//         } if (i == id) {
-//            return console.log('tinka')}
-//      else  {console.log(`nera`)};
-// }
